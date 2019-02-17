0ec1dc41b7e38a7636ad2a85b9a094619b3ce31d
Daniel Berthereau <Daniel.github@Berthereau.net>
2019-02-18 00:00:00 +0100

    Updated packages.

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,44 @@
 'use strict';
 
-var async = require('async');
-var del = require('del');
-var fs = require('fs');
-var glob = require("glob");
-var gulp = require('gulp');
-var rename = require("gulp-rename");
+const gulp = require('gulp');
+const del = require('del');
+const fs = require('fs');
+const glob = require("glob");
+const rename = require("gulp-rename");
+const replace = require('gulp-string-replace');
+
+const bundle = [
+    {
+        'source': 'node_modules/universalviewer/dist/uv-*/**',
+        'dest': 'views/shared/javascripts/uv',
+    },
+];
 
 gulp.task('clean', function(done) {
-    return del('views/shared/javascripts/uv');
-});
-
-gulp.task('sync', function(done) {
-    async.series([
-        function (next) {
-            gulp.src(['node_modules/universalviewer/dist/uv-*/**'])
-            .pipe(gulp.dest('views/shared/javascripts/'))
-            .on('end', next);
-        }
-    ], done);
-});
-
-gulp.task('rename', function(done) {
-    var file = glob.sync('views/shared/javascripts/uv-*/');
-    fs.renameSync(file[0], 'views/shared/javascripts/uv/');
+    bundle.forEach(function (module) {
+        return del.sync(module.dest);
+    });
     done();
 });
 
-gulp.task('default', gulp.series('clean', 'sync', 'rename'));
+gulp.task('sync', function (done) {
+    bundle.forEach(function (module) {
+        gulp.src(module.source)
+            .pipe(gulp.dest(module.dest))
+            .on('end', done);
+    });
+});
+
+// The dist is unknown, so it should be renamed.
+const rename_uv = function(done) {
+    var file = glob.sync('views/shared/javascripts/uv/uv-*/');
+    fs.renameSync(file[0], 'views/shared/javascripts/uv_dist/');
+    del.sync('views/shared/javascripts/uv/');
+    fs.renameSync('views/shared/javascripts/uv_dist/', 'views/shared/javascripts/uv/');
+    done();
+};
+
+gulp.task('default', gulp.series('clean', 'sync', rename_uv));
 
 gulp.task('install', gulp.task('default'));
 
